3879757a614185358e08e3e541567cc052ed2755
Melvyn Harbour <melharbour@gmail.com>
2019-08-22 15:45:28 +0100

    Remove auto paragraphs

--- functions.php
+++ functions.php
@@ -158,6 +158,8 @@ add_filter('widget_text', 'do_shortcode');
 // Custom widgets
 include_once('widgets/wd-contact-widgetplugin.php');
 
+remove_filter( 'acf_the_content', 'wpautop' );
+
 add_action( 'wp_enqueue_scripts', function() {
   wp_dequeue_style( 'wp-block-library' );
 } );
